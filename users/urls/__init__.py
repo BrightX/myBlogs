c49225916863013a3207cbd0bbcf5817563f3661
徐亮亮 <1626769688@qq.com>
2021-03-18 16:07:13 +0800

    用户登录和注册的视图改为CBV(基于类的视图)

--- users/urls/__init__.py
+++ users/urls/__init__.py
@@ -1,12 +1,13 @@
 from django.urls import path
+
 from users import views
 
 app_name = 'users'
 urlpatterns = [
-    path('login', views.user_login, name='login'),
+    path('login', views.UserLoginView.as_view(), name='login'),
+    path('join', views.UserJoinView.as_view(), name='join'),
+    path('isExist', views.UserIsExistView.as_view(), name='isExist'),
     path('logout', views.user_logout, name='logout'),
-    path('join', views.user_join, name='join'),
     path('update', views.user_update, name='update'),
     path('password', views.user_password, name='password'),
-    path('isExist', views.user_is_exist, name='isExist'),
 ]
--- users/views.py
+++ users/views.py
@@ -6,29 +6,36 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.views.generic import TemplateView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from users.models import UserInfo
 from users.utils import get_or_create_author_group, user_perm_init
 
 
-def user_login(request) -> (HttpResponse, JsonResponse):
-    """ 用户登录 """
-    if request.user.is_authenticated:
-        """若用户已登录，则跳过登录界面"""
-        next_url = request.GET.get("next", "/")
-        return redirect(next_url)
+class UserLoginView(TemplateView):
+    """用户登录视图"""
+    template_name = 'users/login.html'
 
-    if request.method == 'GET':
+    def get(self, request, *args, **kwargs):
+        next_url = request.GET.get('next', '/')
+
+        if request.user.is_authenticated:
+            """若用户已登录，则跳过登录界面"""
+            return redirect(next_url)
+
         captcha_hashkey = CaptchaStore.generate_key()  # 生成验证码的hashkey
         captcha_img_url = captcha_image_url(captcha_hashkey)  # 生成验证码图片
-        context = {
+        self.extra_context = {
             'captcha_hashkey': captcha_hashkey,
             'captcha_img_url': captcha_img_url,
-            'next': request.GET.get("next", "/"),
+            'next': next_url,
         }
-        return render(request, 'users/login.html', context=context)
+        return super().get(request, *args, **kwargs)
 
-    if request.method == 'POST':
+    def post(self, request, *args, **kwargs):
+        next_url = self.request.GET.get('next') or self.request.POST.get('next', '/')
         username = request.POST.get('username').strip()
         password = request.POST.get('password').strip()
         hashkey = request.POST.get('captcha_0')
@@ -37,51 +44,50 @@ def user_login(request) -> (HttpResponse, JsonResponse):
         data = {
             'code': -1,
             'msg': 'error',
-            'next': request.GET.get('next') or request.POST.get('next', '/'),
+            'next': next_url,
         }
 
-        if CaptchaStore.objects.get(hashkey=hashkey).response != captcha.lower():
+        captcha_store = CaptchaStore.objects.filter(hashkey=hashkey).first()
+        if not captcha_store or captcha_store.response != captcha.lower():
             data['msg'] = '验证码错误'
-            return JsonResponse(data=data)
+            return JsonResponse(data)
 
-        data['msg'] = '用户名或密码错误'
-        if username and password:
-            user = auth.authenticate(username=username, password=password)
-            if user:
-                data['code'] = 200
-                data['msg'] = "登录成功"
-                auth.login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+        if not username or not password:
+            data['msg'] = '用户名或密码不能为空'
+            return JsonResponse(data)
 
-        return JsonResponse(data=data)
+        user = auth.authenticate(username=username, password=password)
+        if not user:
+            data['msg'] = '用户名或密码错误'
+            return JsonResponse(data)
 
-    return HttpResponse('拒绝访问', status=403)
-
-
-@login_required
-def user_logout(request) -> HttpResponse:
-    """ 退出登录 """
-    auth.logout(request)
-    return redirect("users:login")
+        auth.login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+        data['code'] = 200
+        data['msg'] = "登录成功"
+        return JsonResponse(data)
 
 
-def user_join(request) -> (HttpResponse, JsonResponse):
-    """ 用户注册 """
-    if request.user.is_authenticated:
-        """若用户已登录，则跳过注册界面"""
-        next_url = request.GET.get("next", "/")
-        return redirect(next_url)
+class UserJoinView(TemplateView):
+    """用户注册视图"""
+    template_name = 'users/join.html'
 
-    if request.method == 'GET':
+    def get(self, request, *args, **kwargs):
+        next_url = request.GET.get('next', '/')
+        if request.user.is_authenticated:
+            """若用户已登录，则跳过注册界面"""
+            return redirect(next_url)
+
         captcha_hashkey = CaptchaStore.generate_key()
         captcha_img_url = captcha_image_url(captcha_hashkey)
-        context = {
+        self.extra_context = {
             'captcha_hashkey': captcha_hashkey,
             'captcha_img_url': captcha_img_url,
-            'next': request.GET.get("next", "/"),
+            'next': next_url,
         }
-        return render(request, 'users/join.html', context=context)
+        return super(UserJoinView, self).get(request, *args, **kwargs)
 
-    if request.method == 'POST':
+    def post(self, request, *args, **kwargs):
+        next_url = self.request.GET.get('next') or self.request.POST.get('next', '/')
         username = request.POST.get('username').strip()
         password = request.POST.get('password').strip()
         hashkey = request.POST.get('captcha_0', 'error')
@@ -91,21 +97,22 @@ def user_join(request) -> (HttpResponse, JsonResponse):
 
         data = {
             'code': -1,
+            'next': next_url,
             'msg': 'error',
-            'next': request.GET.get('next') or request.POST.get('next', '/'),
         }
 
-        if CaptchaStore.objects.get(hashkey=hashkey).response != captcha.lower():
+        captcha_store = CaptchaStore.objects.filter(hashkey=hashkey).first()
+        if not captcha_store or captcha_store.response != captcha.lower():
             data['msg'] = '验证码错误'
             return JsonResponse(data=data)
 
-        if not (username and password):
+        if not username or not password:
             data['msg'] = '用户名或密码不能为空'
-            return JsonResponse(data=data)
+            return JsonResponse(data)
 
         if User.objects.filter(username=username).exists():
-            data['msg'] = '该用户ID已存在'
-            return JsonResponse(data=data)
+            data['msg'] = '该用户名已存在'
+            return JsonResponse(data)
 
         user = User.objects.create_user(username=username, password=password)
         group = get_or_create_author_group()
@@ -117,26 +124,32 @@ def user_join(request) -> (HttpResponse, JsonResponse):
         if gender is not None:
             user_info.gender = bool(int(gender))
         user_info.save()
+        auth.login(request, user, backend='django.contrib.auth.backends.ModelBackend')
         data['code'] = 200
         data['msg'] = "注册成功"
-        auth.login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-        return JsonResponse(data=data)
-
-    return HttpResponse('拒绝访问', status=403)
+        return JsonResponse(data)
 
 
-def user_is_exist(request) -> JsonResponse:
+class UserIsExistView(APIView):
     """ 查询用户是否已存在 """
-    username = request.GET.get('username') or request.POST.get('username')
-    data = {
-        'code': 200,
-        'isExist': False,
-        'username': username,
-    }
-    if username:
-        if User.objects.filter(username=username).exists():
-            data['isExist'] = True
-    return JsonResponse(data=data)
+
+    def get(self, *args, **kwargs):
+        username = self.request.query_params.get('username')
+        data = {
+            'code': 200,
+            'isExist': False,
+            'username': username,
+        }
+        if username:
+            data['isExist'] = User.objects.filter(username=username).exists()
+        return Response(data)
+
+
+@login_required
+def user_logout(request):
+    """ 退出登录 """
+    auth.logout(request)
+    return redirect("users:login")
 
 
 @login_required
